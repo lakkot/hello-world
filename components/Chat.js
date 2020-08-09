4e3b09742fc7c71da1b5f89ec66b0ff10011404c
lakkot <40659615+lakkot@users.noreply.github.com>
2020-08-09 22:04:38 +0200

    1013 adding comments, cleaning up

--- components/Chat.js
+++ components/Chat.js
@@ -19,7 +19,7 @@ export default class Chat extends React.Component {
       },
 
     };
-    
+    //firabase access data
     const firebaseConfig = {
       apiKey: "AIzaSyBuY5bi4m_iDquRm4SGJ6ONGMw6xuYkLOA",
       authDomain: "my-project-1530093055040.firebaseapp.com",
@@ -30,35 +30,27 @@ export default class Chat extends React.Component {
       appId: "1:404822874074:web:59ebf927e425dff38752e3",
       measurementId: "G-2FR2JSWQSS"
     }
-  
-  if (!firebase.apps.length){
-    firebase.initializeApp(firebaseConfig);
-    };
-  this.referenceMessages = firebase.firestore().collection('myChatApp');
-  //this.referenceMessagesUser = firebase.firestore().collection('myChatApp').where("uid", "==", this.state.uid);
-  
+    //call database from firestone
+    if (!firebase.apps.length){
+      firebase.initializeApp(firebaseConfig);
+      };
+    this.referenceMessages = firebase.firestore().collection('myChatApp');
+    //this.referenceMessagesUser = firebase.firestore().collection('myChatApp').where("uid", "==", this.state.uid);
   }
+
   //feed messages to state on component mount
   componentDidMount() {
-
-    
-    
- 
-
-    // listen for collection changes for current user
-    //this.unsubscribeMessagesUser = this.referenceMessagesUser.onSnapshot(this.onCollectionUpdate);
-
+    //if noo user data, sign in anonymously and get uid
     this.authUnsubscribe = firebase.auth().onAuthStateChanged(async (user) => {
       if (!user) {
         await firebase.auth().signInAnonymously();
       }
-    
-  //update user state with currently active user data
-  this.setState({
-    uid: user.uid,
-    loggedInText: 'Hello there',
-  });
-      // create a reference to the active user's documents (shopping lists)
+      //update user state with currently active user data
+      this.setState({
+        uid: user.uid,
+        loggedInText: 'Hello there',
+      });
+      // add user data
       this.setState({
         user: {
           name: this.props.route.params.name,
@@ -66,53 +58,55 @@ export default class Chat extends React.Component {
         },
       });
     });
-
-this.unsubscribe = this.referenceMessages.onSnapshot(this.onCollectionUpdate);
-
+    // listen for collection changes for current user
+    this.unsubscribe = this.referenceMessages.onSnapshot(this.onCollectionUpdate);
+    //this.unsubscribeMessagesUser = this.referenceMessagesUser.onSnapshot(this.onCollectionUpdate);
   }
   
   componentWillUnmount() {
     this.unsubscribe();
     //this.unsubscribeMessagesUser();
- }
-
- onCollectionUpdate = (querySnapshot) => {
-  const messages = [];
-  // loop through documents
-  querySnapshot.forEach((doc) => {
-    // get data snapshot
-    const data = doc.data();
-    messages.push({
-      _id: data._id,
-      text: data.text.toString(),
-      createdAt: data.createdAt.toDate(),
-      user: {
-        _id: data.user._id,
-        name: data.user.name,
-      },
-    });
-    messages.sort((a, b) => b.createdAt - a.createdAt)
-  });
-  this.setState({
-    messages,
-  });
-  
-  if (this.state.messages.length === 0) {
-    var firstMessage = [
-      {
-        _id: 1,
-        text: `Wellcome to the chat App`,
-        createdAt: new Date(),
-        user: {
-          _id: 2,
-          name: 'Chatbot',
-        },
-      },
-    ];
-    this.onSend(firstMessage);
   }
-  
-};
+
+  //update state wehn new messages appear
+  onCollectionUpdate = (querySnapshot) => {
+    const messages = [];
+    // loop through documents
+    querySnapshot.forEach((doc) => {
+      // get data snapshot
+      const data = doc.data();
+      messages.push({
+        _id: data._id,
+        text: data.text.toString(),
+        createdAt: data.createdAt.toDate(),
+        user: {
+          _id: data.user._id,
+          name: data.user.name,
+        },
+      });
+      //sort messages by date added
+      messages.sort((a, b) => b.createdAt - a.createdAt)
+    });
+    this.setState({
+      messages,
+    });
+    //add a welcome message if thread is empty
+    if (this.state.messages.length === 0) {
+      var firstMessage = [
+        {
+          _id: 1,
+          text: `Wellcome to the chat App`,
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'Chatbot',
+          },
+        },
+      ];
+      this.onSend(firstMessage);
+    }
+  };
+
   //add new messages to state
   onSend(messages = []) {
     this.setState(
@@ -125,7 +119,7 @@ this.unsubscribe = this.referenceMessages.onSnapshot(this.onCollectionUpdate);
     );
   }
 
-
+  //add new messages to firestone database
   addMessages = () => {
     const message = this.state.messages[0];
     this.referenceMessages.add({
@@ -136,6 +130,7 @@ this.unsubscribe = this.referenceMessages.onSnapshot(this.onCollectionUpdate);
     });
   };
 
+  //add attributes to bubbles
   renderBubble(props) {
     return (
       <Bubble
@@ -145,7 +140,7 @@ this.unsubscribe = this.referenceMessages.onSnapshot(this.onCollectionUpdate);
             backgroundColor: '#121F67'
           },
           left: {
-            backgroundColor: '#FFEBD0'
+              backgroundColor: '#FFEBD0'
           }
         }}
       />
@@ -153,10 +148,8 @@ this.unsubscribe = this.referenceMessages.onSnapshot(this.onCollectionUpdate);
   }
   
   render() {
-    
     let name = this.props.route.params.name; // OR ...
     let color = this.props.route.params.color;
-
     this.props.navigation.setOptions({ title: name });
 
     return (
@@ -183,27 +176,3 @@ const styles = StyleSheet.create({
     flex: 1
   },
 })
-
-
-
-/*
-<View style={styles.container}>
-
-<Text>{this.state.loggedInText}</Text>
-
-<Text style={styles.text}>All Shopping lists</Text>
-<FlatList
-    data={this.state.lists}
-    renderItem={({ item }) => 
-      <Text style={styles.item}>{item.name}: {item.items}</Text>}
-  />
-
-<Button 
-  onPress={() => {
-    this.addList();
-  }}
-  title = "Add something"
-/>
-</View>
-
-*/
